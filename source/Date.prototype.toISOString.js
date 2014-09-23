866cbe386464c2fd89476fcc7c18d4bdd02fa213
a-t <at@slavdelo.ru>
2014-09-23 15:51:50 +0400

    Date.toISOString() date fix for IE
    Unfortunately `date.toUTCString()` in IE < 11 returns dates without leading zeros (e.g. `'Fri, 7 Mar 2014 08:32:15 UTC'`). Here comes a workaround for that.

--- source/Date.prototype.toISOString.js
+++ source/Date.prototype.toISOString.js
@@ -1,4 +1,4 @@
 // Date.prototype.toISOString
 Date.prototype.toISOString = function toISOString() {
-	return ((this.getUTCMonth() + 1) / 100 + this.toUTCString() + this * 1).replace(/..(..).+?(\d+)\D+(\d+).(\S+).*(...)/,'$3-$1-$2T$4.$5Z');
+	return ('' + (this.getUTCMonth() + 1) / 100 + this.getUTCDate() / 100 + this.toUTCString() + this * 1).replace(/..(..)..(..).+?(\d{4}).(\S+).*(...)/, '$3-$1-$2T$4.$5Z');
 };
